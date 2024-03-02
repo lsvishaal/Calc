18d2b2c89d6652e4ba95efd41c5bd1b95991a817
Kinetik30 <sivanath3008@gmail.com>
2024-03-02 20:44:08 +0530

    test

---
+++ main.py
@@ -0,0 +1,12 @@
+
+num1=int(input('Enter a number: '))
+num2=int(input('Enter a number: '))
+sum=num1+num2
+print('Addition is :',sum)
+
+sub=num1-num2
+print('Subtraction is: ',sub)
+mul=num1*num2
+print('Multiplication is: ',mul)
+div=num1/num2
+print('Subtraction is: ',div)
